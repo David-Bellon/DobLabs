120c0536a7d39f3a8443db001e07fb362ac976d9
David Bellon <dadbc16@gmail.com>
2023-01-28 23:41:28 +0100

    not bad

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
   }
 
+
+var url = new URL("https://api4.binance.com/api/v3/ticker/price");
 const ethereumButton = document.getElementById("enableEthereumButton");
+var eth_id = document.getElementById("eth");
 var user_account;
 
 ethereumButton.addEventListener('click', () => {
@@ -17,3 +20,14 @@ async function getAccount() {
     user_account = account;
     ethereumButton.innerHTML = account.substring(0, 20) + "...";
   }
+
+
+setInterval(function() {
+  let params = {symbol: "ETHUSDT"};
+  url.search = new URLSearchParams(params).toString();
+  fetch(url).then((response) => {
+    response.json().then((myjson) => {
+      eth_id.innerHTML = myjson["price"];
+    });
+  });
+}, 1000);
