6ce3ff3800c9990a8c822f984dcab5d68e554ea4
David Bellon <dadbc16@gmail.com>
2023-01-29 13:12:11 +0100

    price change and more

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const web3 = new Web3(Web3.givenProvider);
+const tickets = ["btcusdt", "ethusdt"]
 if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
   }
 
 
-var url = new URL("https://api4.binance.com/api/v3/ticker/price");
+var url = new URL("https://api4.binance.com/api/v3/ticker?");
 const ethereumButton = document.getElementById("enableEthereumButton");
 var user_account;
 
@@ -22,12 +23,24 @@ async function getAccount() {
 
 
 setInterval(function() {
-  let params = {symbol: "ETHUSDT"};
-  var eth_id = document.getElementById("eth").getElementsByClassName("price-asset")[0];
-  url.search = new URLSearchParams(params).toString();
-  fetch(url).then((response) => {
+  let params = 'symbols=%5B"ETHUSDT","BTCUSDT"%5D';
+  var search_url = url + params;
+  fetch(search_url).then((response) => {
     response.json().then((myjson) => {
-      eth_id.innerHTML = "ETH/USD: " + String(Number(myjson["price"]).toFixed(2)) + "$";
+      for (var i=0; tickets.length; i++){
+        var ticket_id = document.getElementById(tickets[i]);
+        var price_class = ticket_id.getElementsByClassName("price-asset")[0];
+        var change_per = ticket_id.getElementsByClassName("change-day")[0];
+        price_class.innerHTML = String(tickets[i].toUpperCase() + ":  " + Number(myjson[i]["lastPrice"]).toFixed(3) + "$");
+        if (parseFloat(myjson[i]["priceChangePercent"]) < 0){
+          change_per.style.color = "red";
+          change_per.innerHTML = String("24H % Change:  " + "-" + myjson[i]["priceChangePercent"]) + "%";
+        }
+        else{
+          change_per.style.color = "green";
+          change_per.innerHTML = String("24H % Change:  " + "+" + myjson[i]["priceChangePercent"]) + "%";
+        }
+      }
     });
   });
 }, 1000);
